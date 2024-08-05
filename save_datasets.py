0c04f7e4a885c524b36c2dd39841801218b7d4a7
jangseop <kgh010529@naver.com>
2024-08-05 23:02:50 +0900

    config deepspeed 0805

--- save_datasets.py
+++ save_datasets.py
@@ -8,13 +8,15 @@ for i in range(1,n_fixed_file+1):
     with open(f"c_fixes_raw/c{i}.c", 'r') as file:
         content = file.read()
         c_fixes_json.append({
-            "idx": str(i),
-            "content": content
+            "label": content,
+            "input_ids": content
         })
 
-with open("./c_fixes.json", 'w') as new_file:
+with open("./c_fixes.jsonl", 'w') as new_file:
     json.dump(c_fixes_json, new_file, indent=4)
     
     
-dataset = load_dataset("json", data_files="./c_fixes.json", split="train")
+dataset = load_dataset("json", data_files="./c_fixes.jsonl", split="train")
 print(dataset)
+
+# codegen-2B trainable params: 2,621,440 || all params: 2,781,977,600 || trainable%: 0.09422937122139301
--- setup_peft.py
+++ setup_peft.py
@@ -3,6 +3,7 @@ import json
 import torch
 import argparse
 import deepspeed
+import pandas as pd
 from deepspeed import get_accelerator
 
 from torch.utils.data import DataLoader, TensorDataset 
@@ -18,32 +19,43 @@ from transformers import (
 from peft import get_peft_model, LoraConfig, TaskType
 
 class CustomDataset(Dataset):
-    def __init__(self, data, targets):
-        self.data = data
-        self.targets = targets
+    def __init__(self, data):
+        self._data = data
         
     def __len__(self):
-        return len(self.data)
+        return len(self._data)
     def __getitem__(self, idx):
-        return self.data[idx], self.targets[idx]
+        print("[custom datset] idx: ", idx)
+        if isinstance(idx, list): idx = idx[0]
+        print("[custom datset - parsing] idx: ", idx)
+        return (
+            torch.LongTensor(self._data[idx]['input_ids']), 
+            torch.LongTensor(self._data[idx]['input_ids'])
+        )
 
-model_path = "DDIDU/ETRI_CodeLLaMA_7B_CPP"
+# model_path = "DDIDU/ETRI_CodeLLaMA_7B_CPP"
+model_path = "Salesforce/codegen-350M-multi"
 dataset_path = "./c_fixes.json"
 
 # with open(dataset_path, 'r') as file:
 #     c_fixes_json = json.load(file)
 
-dataset = load_dataset("json", data_files="./c_fixes.json", split="train")
+with open('./c_fixes.jsonl', 'r') as file:
+    data = json.load(file)
 
-device_map = {"": torch.cuda.current_device()}
-model = AutoModelForCausalLM.from_pretrained(model_path, device_map=device_map, trust_remote_code=True)
+# dataset = load_dataset("json", data_files="./c_fixes.json", split="train")
+dataset = Dataset.from_pandas(pd.DataFrame(data))
+# dataset = load_dataset('imdb')
+
+model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True)
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 tokenizer.pad_token = tokenizer.eos_token
 
 def tokenize_funtion(examples):
-    return tokenizer(examples['content'], max_length=512, padding="max_length", truncation=True)
+    return tokenizer(examples['input_ids'], max_length=512, padding="max_length", truncation=True)
 
-tokenized = dataset.map(tokenize_funtion, batched=True)
+tokenized_datasets = dataset.map(tokenize_funtion, batched=True)
+tokenized_datasets.set_format("torch")
 # tokenized['label'] = tokenized['input_ids']
 
 peft_config = LoraConfig(
@@ -59,10 +71,14 @@ model.print_trainable_parameters()
 
 # train configuration (with DeepSpeed)
 
-ds_dataset = CustomDataset(tokenized['input_ids'], tokenized['input_ids'])
-data_loader = DataLoader(ds_dataset)
+
+# ds_dataset = CustomDataset(tokenized)
+data_loader = DataLoader(tokenized_datasets, batch_size=16, shuffle=True)
 
 ds_config = {
+    "train_batch_size": 4,
+    "train_micro_batch_size_per_gpu": 1,
+    "gradient_accumulation_steps": 1,
     "optimizer": {
         "type": "Adam",
         "params": {"lr": 0.001}
@@ -75,23 +91,29 @@ ds_config = {
         "offload_param": {
             "device": "[cpu|nvme]"
         }
+    },
+    "fp16": {
+        "enabled": True
     }
 }
 
 model_engine, optimizer, _, _ = deepspeed.initialize(
     model=model,
-    training_data=ds_dataset,
+    model_parameters=model.parameters(),
+    training_data=tokenized_datasets,
     config=ds_config
 )
 
-
-for step, batch in enumerate(data_loader):
+cnt = 1
+for batch in data_loader:
     #forward() method
-    loss = model_engine(batch)
+    outputs = model_engine(**batch)
 
     #runs backpropagation
-    model_engine.backward(loss)
+    model_engine.backward(outputs.loss)
 
     #weight update
     model_engine.step()
+    cnt += 1
+    print("step # ", cnt)
     
